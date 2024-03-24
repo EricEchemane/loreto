bba1286d8c46f4df38e3eccd62da9f60669ad100
ericechemane <eechemane29@gmail.com>
2024-03-24 21:22:24 +0800

    vehicle details

--- app/(public)/dashboard/@admin/vehicles/[id]/VehicleDetails.tsx
+++ app/(public)/dashboard/@admin/vehicles/[id]/VehicleDetails.tsx
@@ -1,16 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { cn } from '@/lib/utils'
+import { cn, formatDate } from '@/lib/utils'
 import { Vehicle } from '@prisma/client'
 import { ArrowLeftIcon } from '@radix-ui/react-icons'
-import Image from 'next/image'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { VehicleStatusLabel } from '@/common/constants/business'
 export default function VehicleDetails({ data }: { data: Vehicle }) {
   const searchParams = useSearchParams()
   const router = useRouter()
@@ -56,58 +64,112 @@ export default function VehicleDetails({ data }: { data: Vehicle }) {
         </Button>
       </header>
 
-      <div className='grid grid-cols-12 p-4 gap-4'>
-        <Card className='col-span-6 shadow-none'>
-          <CardHeader>
-            <CardTitle className='text-base'>Basic Information</CardTitle>
-          </CardHeader>
-          <CardContent className='space-y-4'>
-            <div className='space-y-1'>
-              <Label
-                htmlFor='vehicle_name'
-                className='small'
-              >
-                Vehicle Name
-              </Label>
-              <Input
-                ref={(ref) => !readOnly && ref?.focus()}
-                id='vehicle_name'
-                defaultValue={data.name}
-                name='name'
-                readOnly={readOnly}
-              />
-            </div>
-            <div className='space-y-1'>
-              <Label htmlFor='model'>Model</Label>
-              <Input
-                id='model'
-                defaultValue={data.model}
-                name='model'
-                readOnly={readOnly}
-              />
-            </div>
-            <div className='space-y-1'>
-              <Label htmlFor='plateNumber'>Plate Number</Label>
-              <Input
-                id='plateNumber'
-                defaultValue={data.plateNumber}
-                name='plateNumber'
-                readOnly={readOnly}
-              />
-            </div>
-          </CardContent>
-        </Card>
+      <div className='grid grid-cols-12 p-4 gap-4 w-[850px] m-auto'>
+        <div className='col-span-7 space-y-4'>
+          <Card className='shadow-none'>
+            <CardHeader>
+              <CardTitle className='text-base'>Vehicle Information</CardTitle>
+            </CardHeader>
+            <CardContent className='space-y-4'>
+              <div className='space-y-1'>
+                <Label
+                  htmlFor='vehicle_name'
+                  className='small'
+                >
+                  Vehicle Name
+                </Label>
+                <Input
+                  ref={(ref) => !readOnly && ref?.focus()}
+                  id='vehicle_name'
+                  defaultValue={data.name}
+                  name='name'
+                  readOnly={readOnly}
+                />
+              </div>
+              <div className='space-y-1'>
+                <Label htmlFor='model'>Model</Label>
+                <Input
+                  id='model'
+                  defaultValue={data.model}
+                  name='model'
+                  readOnly={readOnly}
+                />
+              </div>
+              <div className='space-y-1'>
+                <Label htmlFor='plateNumber'>Plate Number</Label>
+                <Input
+                  id='plateNumber'
+                  defaultValue={data.plateNumber}
+                  name='plateNumber'
+                  readOnly={readOnly}
+                />
+              </div>
+            </CardContent>
+          </Card>
 
-        <Card className='col-span-6 relative shadow-none'>
-          <Image
-            fill
-            priority
+          <Card className='shadow-none'>
+            <CardHeader>
+              <CardTitle className='text-base'>History</CardTitle>
+            </CardHeader>
+            <CardContent className='space-y-4'>
+              <div className='space-y-1'>
+                <Label htmlFor='lastMaintenance'>Last Maintenance</Label>
+                <Input
+                  id='lastMaintenance'
+                  defaultValue={
+                    data.lastMaintenance
+                      ? formatDate(data.lastMaintenance.toString())
+                      : undefined
+                  }
+                  name='lastMaintenance'
+                  readOnly={readOnly}
+                />
+              </div>
+              <div className='space-y-1'>
+                <Label htmlFor='purchaseDate'>Purchased On</Label>
+                <Input
+                  id='purchaseDate'
+                  defaultValue={formatDate(data.purchaseDate.toString())}
+                  name='purchaseDate'
+                  readOnly={readOnly}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className='col-span-5 space-y-4'>
+          <img
             alt={data.name}
             src={data.photoUrl}
-            className='object-contain rounded-md'
+            className='rounded-xl border'
             sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           />
-        </Card>
+
+          <Card className='shadow-none p-5'>
+            <div className='space-y-1'>
+              <Label>Status</Label>
+              <Select
+                defaultValue={data.status.toString()}
+                disabled={readOnly}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder='Status' />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.entries(VehicleStatusLabel).map(([key, value]) => (
+                    <SelectItem
+                      key={key}
+                      value={key}
+                    >
+                      {value}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </Card>
+        </div>
       </div>
     </div>
   )
--- app/(public)/dashboard/@admin/vehicles/columns.tsx
+++ app/(public)/dashboard/@admin/vehicles/columns.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client'
 
-import { VehicleStatusLabel } from "@/common/constants/business";
-import { VehicleStatus } from "@/common/enums/enums.db";
-import { formatDate } from "@/lib/utils";
-import { Vehicle } from "@prisma/client";
-import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
+import { VehicleStatusLabel } from '@/common/constants/business'
+import { VehicleStatus } from '@/common/enums/enums.db'
+import { formatDate } from '@/lib/utils'
+import { Vehicle } from '@prisma/client'
+import { ColumnDef } from '@tanstack/react-table'
+import { Button } from '@/components/ui/button'
 
 import {
   DropdownMenu,
@@ -13,16 +13,16 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from '@/components/ui/dropdown-menu'
 
-import { DotsHorizontalIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
+import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const vehiclesTableColumns: ColumnDef<Vehicle>[] = [
   {
-    accessorKey: "photoUrl",
-    header: "Image",
+    accessorKey: 'photoUrl',
+    header: 'Image',
     cell: ({ row }) => {
       return (
         <Image
@@ -31,55 +31,58 @@ export const vehiclesTableColumns: ColumnDef<Vehicle>[] = [
           src={row.original.photoUrl}
           width={50}
           height={50}
-          className="h-auto w-auto rounded-md"
+          className='h-auto w-auto rounded-md'
         />
-      );
+      )
     },
   },
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: 'name',
+    header: 'Name',
   },
   {
-    accessorKey: "model",
-    header: "Model",
+    accessorKey: 'model',
+    header: 'Model',
   },
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: 'status',
+    header: 'Status',
     cell: ({ row }) => VehicleStatusLabel[row.original.status as VehicleStatus],
   },
   {
-    accessorKey: "lastMaintenance",
-    header: "Last Maintenance",
+    accessorKey: 'lastMaintenance',
+    header: 'Last Maintenance',
     cell: ({ row }) => {
-      const date = row.original.lastMaintenance;
-      return date ? formatDate(date.toString()) : "";
+      const date = row.original.lastMaintenance
+      return date ? formatDate(date.toString()) : ''
     },
   },
   {
-    id: "actions",
+    id: 'actions',
     cell: ({ row }) => {
-      const vehicle = row.original;
+      const vehicle = row.original
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
+            <Button
+              variant='ghost'
+              className='h-8 w-8 p-0'
+            >
+              <span className='sr-only'>Open menu</span>
+              <DotsHorizontalIcon className='h-4 w-4' />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
+          <DropdownMenuContent align='end'>
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem>
-              <Link href={`/dashboard/vehicles/${vehicle.id}`}>
-                View details
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <Link href={`/dashboard/vehicles/${vehicle.id}`}>
+              <DropdownMenuItem>View details</DropdownMenuItem>
+            </Link>
+            <Link href={`/dashboard/vehicles/${vehicle.id}?action=edit`}>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+            </Link>
           </DropdownMenuContent>
         </DropdownMenu>
-      );
+      )
     },
   },
-];
+]
