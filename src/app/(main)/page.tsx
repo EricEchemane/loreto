12146a75737cfc71f3127c6e34b2f7f5042e231e
ericechemane <eechemane29@gmail.com>
2024-11-21 12:12:16 +0800

    animated landing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
+import LoretoTradingH1, {
+  LoretoTradingAbout,
+  LoretoTradingImage,
+} from './animation-components'
 import Navbar from './Navbar'
 
 export default function Home() {
@@ -9,23 +12,11 @@ export default function Home() {
       <main className='max-w-5xl m-auto p-3 mt-4'>
         <div className='grid grid-cols-2'>
           <div className='mt-16'>
-            <h1>
-              Loreto <span className='text-rose-600'>Trading</span>
-            </h1>
-            <p className='text-muted-foreground mt-3'>
-              We are a company that specializes in providing packaging
-              solutions. Our unique feature allows for the resizing of boxes and
-              the addition of layout markings. With our services, you can
-              customize boxes to perfectly fit your specific size requirements,
-              complete with accurate markings for easy assembly and
-              organization.
-            </p>
+            <LoretoTradingH1 />
+            <LoretoTradingAbout />
           </div>
           <div className='mt-8'>
-            <img
-              src='/order_delivered.svg'
-              alt='box'
-            />
+            <LoretoTradingImage />
           </div>
         </div>
       </main>
