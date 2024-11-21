9445f2bfabbd7afb15c8d79115c7ea80d4cfecb6
ericechemane <eechemane29@gmail.com>
2024-11-21 12:06:12 +0800

    home page redesign

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Navbar from './Navbar'
 
 export default function Home() {
@@ -6,15 +7,26 @@ export default function Home() {
       <Navbar />
 
       <main className='max-w-5xl m-auto p-3 mt-4'>
-        <div>
-          <h1>Loreto Trading</h1>
-          <p className='text-muted-foreground mt-3'>
-            We are a company that specializes in providing packaging solutions.
-            Our unique feature allows for the resizing of boxes and the addition
-            of layout markings. With our services, you can customize boxes to
-            perfectly fit your specific size requirements, complete with
-            accurate markings for easy assembly and organization.
-          </p>
+        <div className='grid grid-cols-2'>
+          <div className='mt-16'>
+            <h1>
+              Loreto <span className='text-rose-600'>Trading</span>
+            </h1>
+            <p className='text-muted-foreground mt-3'>
+              We are a company that specializes in providing packaging
+              solutions. Our unique feature allows for the resizing of boxes and
+              the addition of layout markings. With our services, you can
+              customize boxes to perfectly fit your specific size requirements,
+              complete with accurate markings for easy assembly and
+              organization.
+            </p>
+          </div>
+          <div className='mt-8'>
+            <img
+              src='/order_delivered.svg'
+              alt='box'
+            />
+          </div>
         </div>
       </main>
     </div>
