6f08d44fc133701143514ebea045e501c1e7929b
ericechemane <eechemane29@gmail.com>
2025-05-31 19:58:53 +0800

    new mig: tenant model

---
+++ src/app/(main)/dashboard/tenants/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <header className='p-4'>
+        <h2>Tenants</h2>
+      </header>
+      <div className='p-4'></div>
+    </div>
+  )
+}
